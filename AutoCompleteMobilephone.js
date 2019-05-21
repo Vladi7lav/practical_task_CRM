10f7ae58fe05f7bf9e0e2f72781d382288e28e79
Vladislav Tsirulev <Vladislav.Tsirulev@columbusglobal.com>
2019-05-21 14:20:12 +0300

    Create AutoCompleteMobilephone.js

---
+++ AutoCompleteMobilephone.js
@@ -0,0 +1,27 @@
+function Auto_Complete_Mobilephone() {
+    var to = Xrm.Page.getAttribute("to").getValue();    
+    var phonenumber = Xrm.Page.getAttribute("new_phone_number_recipient");
+    if (to[0] != undefined)
+    {
+        var Guid = to[0].id.substr(1, 36);
+        var req = new XMLHttpRequest();
+        req.open("GET", Xrm.Page.context.getClientUrl() + "/api/data/v8.0/" + to[0].entityType + "s(" + Guid + ")?$select=telephone1", true);
+        req.setRequestHeader("OData-MaxVersion", "4.0");
+        req.setRequestHeader("OData-Version", "4.0");
+        req.setRequestHeader("Accept", "application/json");
+        req.setRequestHeader("Content-Type", "application/json; charset=utf-8");
+        req.setRequestHeader("Prefer", "odata.include-annotations=\"*\"");
+        req.onreadystatechange = function () {
+            if (this.readyState === 4) {
+                req.onreadystatechange = null;
+                if (this.status !== 200) {
+                    Xrm.Utility.alertDialog(this.statusText);
+                } else {
+                    var result = JSON.parse(this.response);
+                    phonenumber.setValue(result["telephone1"]);
+                }
+            }
+        };
+        req.send();
+    }
+} 
