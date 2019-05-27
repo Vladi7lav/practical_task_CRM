29660b1b69c29b20c4b3cfe8f3b349e2cf45b25a
Vladislav Tsirulev <Vladislav.Tsirulev@columbusglobal.com>
2019-05-27 09:33:13 +0300

    Update
    Update

--- SentMSG/SentMSG/SentMSG.cs
+++ SentMSG/SentMSG/SentMSG.cs
@@ -20,48 +20,51 @@ namespace SentMSG
         protected override void Execute(CodeActivityContext executionContext)
         {
             //System.Diagnostics.Debugger.Launch();            
-            EntityReference SMSref = this.SMS.Get(executionContext);
 
             IWorkflowContext context = executionContext.GetExtension<IWorkflowContext>();
             IOrganizationServiceFactory serviceFactory = executionContext.GetExtension<IOrganizationServiceFactory>();
             IOrganizationService service = serviceFactory.CreateOrganizationService(context.InitiatingUserId);
 
-            Entity SMSEntity;
+            Guid SMSid = context.PrimaryEntityId;
+            String SMSType = context.PrimaryEntityName;
+            ColumnSet attribute = new ColumnSet(new string[] { "new_phone_number_recipient", "new_message", "new_messageid"});
 
-            RetrieveRequest retrieveRequest = new RetrieveRequest();
-            retrieveRequest.ColumnSet = new ColumnSet(new string[] { "new_phone_number_recipient", "new_message" });
-            retrieveRequest.Target = SMSref;
-            RetrieveResponse retrieveResponse = (RetrieveResponse)service.Execute(retrieveRequest);
-            SMSEntity = retrieveResponse.Entity as Entity;
+            Entity SMSEntity = service.Retrieve(SMSType, SMSid, attribute);
 
-            SMSEntity.Attributes["new_messageid"] = new Random().Next(0, 100);
-            SMSEntity.Attributes["serviceid"] = SMSref;
+            SMSEntity.Attributes["new_messageid"] = new Random().Next(0, 10000);
             service.Update(SMSEntity);
 
-            XmlWriter xmlWriter = XmlWriter.Create(@"C:\SMS.xml");
+            Boolean checkWrite = false;
+            try
+            {
+                XmlWriter xmlWriter = XmlWriter.Create(@"\\CRM-TRAIN\Shared\SMS.xml");
 
-            xmlWriter.WriteStartDocument();
-            xmlWriter.WriteStartElement("SMS");
+                xmlWriter.WriteStartDocument();
+                xmlWriter.WriteStartElement("SMS");
 
-            xmlWriter.WriteStartElement("Id message");
-            xmlWriter.WriteString($"{SMSEntity.Attributes["id"]}");
-            xmlWriter.WriteEndElement();
+                xmlWriter.WriteStartElement("Id message");
+                xmlWriter.WriteString($"{SMSEntity.Attributes["new_messageid"]}");
+                xmlWriter.WriteEndElement();
 
-            xmlWriter.WriteStartElement("Phonenumber");
-            xmlWriter.WriteString($"{SMSEntity.Attributes["new_phone_number_recipient"]}");
-            xmlWriter.WriteEndElement();
+                xmlWriter.WriteStartElement("Phonenumber");
+                xmlWriter.WriteString($"{SMSEntity.Attributes["new_phone_number_recipient"]}");
+                xmlWriter.WriteEndElement();
 
-            xmlWriter.WriteStartElement("Message");
-            xmlWriter.WriteString($"{SMSEntity.Attributes["new_message"]}");
-            xmlWriter.WriteEndElement();            
+                xmlWriter.WriteStartElement("Message");
+                xmlWriter.WriteString($"{SMSEntity.Attributes["new_message"]}");
+                xmlWriter.WriteEndElement();
 
-            xmlWriter.WriteEndDocument();
-            xmlWriter.Close();
+                xmlWriter.WriteEndDocument();
+                xmlWriter.Close();
+                checkWrite = true;
+            }
+            catch (Exception e)
+            {
+                throw new InvalidWorkflowException(e.Message.ToString());
+            }
+            if (checkWrite)
+                service.Update(SMSEntity);
         }
-        [RequiredArgument]
-        [Input("Sent MSG")]
-        [ReferenceTarget("new_sms")]
-        public InArgument<EntityReference> SMS { get; set; }
     }
 }
 
