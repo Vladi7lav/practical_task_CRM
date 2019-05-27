73f420310aa5a5e411ba64ca6a81afd5515bd1f0
Vladislav Tsirulev <Vladislav.Tsirulev@columbusglobal.com>
2019-05-27 14:53:10 +0300

    Update CheckCorrectNumber.cs

--- CheckCorrectNumber/CheckCorrectNumber/CheckCorrectNumber.cs
+++ CheckCorrectNumber/CheckCorrectNumber/CheckCorrectNumber.cs
@@ -8,6 +8,7 @@ using System.Net;
 using System.ServiceModel;
 using System.ServiceModel.Description;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace CheckCorrectNumber
@@ -28,7 +29,8 @@ namespace CheckCorrectNumber
                     if(SMS.Contains("new_phone_number_recipient") && !String.IsNullOrEmpty((string)SMS.Attributes["new_phone_number_recipient"]))
                     {
                         String phonenumber = (string)SMS.Attributes["new_phone_number_recipient"];
-                        if((!phonenumber.Substring(0, 2).Equals("+7")) || phonenumber.Length != 12)
+                        Regex regexObj = new Regex(@"[^\d]");
+                        if ((!phonenumber.Substring(0, 2).Equals("+7")) || regexObj.Replace(phonenumber, "").Length != 11)
                         {
                             throw new InvalidPluginExecutionException("Sorry phone number incorrect. \n The phone number must be 11 digits and begin with +7");
                         }
