15f7ac4a15bc6a227745faaa92ed4b1474dcab63
Vladislav Tsirulev <Vladislav.Tsirulev@columbusglobal.com>
2019-06-06 13:50:59 +0300

    Update AutoCompleteMobilephone.js

--- AutoCompleteMobilephone.js
+++ AutoCompleteMobilephone.js
@@ -1,31 +1,62 @@
-function Auto_Complete_Mobilephone() {
-    var to = Xrm.Page.getAttribute("to").getValue();    
-    var phonenumber = Xrm.Page.getAttribute("new_phone_number_recipient");
-    if (to == null || to.length == 0) return;
-    else
-    {
-        var Guid = to[0].id.substr(1, 36);
-        var req = new XMLHttpRequest();
-        req.open("GET", Xrm.Page.context.getClientUrl() + "/api/data/v8.0/" + to[0].entityType + "s(" + Guid + ")?$select=telephone1", true);
-        req.setRequestHeader("OData-MaxVersion", "4.0");
-        req.setRequestHeader("OData-Version", "4.0");
-        req.setRequestHeader("Accept", "application/json");
-        req.setRequestHeader("Content-Type", "application/json; charset=utf-8");
-        req.setRequestHeader("Prefer", "odata.include-annotations=\"*\"");
-        req.onreadystatechange = function () {
-            if (this.readyState !== 4) return;
-            else
-            {
-                req.onreadystatechange = null;
-                if (this.status === 200) {
-                    var result = JSON.parse(this.response);
-                    phonenumber.setValue(result["telephone1"]);                        
-                }
-                else {
-                    Xrm.Utility.alertDialog(this.statusText);
-                }
+// JavaScript source code
+function Auto_Complete_Mobilephone()
+{
+    var to = Xrm.Page.getAttribute("to").getValue();
+    var phonenumberEl = Xrm.Page.getAttribute("new_phone_number_recipient");
+
+    if (to != null && to.length != 0) {
+        var phonenumber;
+        var repeat = false;
+        var Guid, mass = [];
+        
+        var j = 0;
+        setPhoneNumber = function (recipient, callback)
+        {
+            phonenumber = phonenumberEl.getValue();
+            if (phonenumber != null) {
+                mass = phonenumber.split(/s*;s*[^$]/);
             }
+
+            Guid = recipient.id.substr(1, 36);
+            var req = new XMLHttpRequest();
+            req.open("GET", Xrm.Page.context.getClientUrl() + "/api/data/v8.0/" + recipient.entityType + "s(" + Guid + ")?$select=telephone1", true);
+            req.setRequestHeader("OData-MaxVersion", "4.0");
+            req.setRequestHeader("OData-Version", "4.0");
+            req.setRequestHeader("Accept", "application/json");
+            req.setRequestHeader("Content-Type", "application/json; charset=utf-8");
+            req.setRequestHeader("Prefer", "odata.include-annotations=\"*\"");
+            req.onreadystatechange = function () {
+                if (this.readyState === 4) {
+                    req.onreadystatechange = null;
+                    if (this.status === 200) {
+                        var result = JSON.parse(this.response);
+                        for (var i = 0; i < mass.length; i++)
+                        {
+                            if (result["telephone1"] == mass[i]) {
+                                repeat = true;
+                                break;
+                            }
+                            else if (i == mass.length - 1)
+                                repeat = false;
+                        }
+                        if (!repeat) {
+                            phonenumber = (phonenumber === null) ? "" : (phonenumber.search(/\s*;\s*$/)===-1 ? (phonenumber + "; "):phonenumber);
+                            phonenumberEl.setValue(phonenumber + result["telephone1"] + "; ");
+                        }
+                        callback();
+                    }
+                    else {
+                        Xrm.Utility.alertDialog(this.statusText);
+                    }
+                }
+            };
+            req.send();
         };
-        req.send();
+        function callback() {
+            j++;
+            if (j < to.length)
+                setPhoneNumber(to[j], callback);
+        }
+        setPhoneNumber(to[0], callback);
     }
 }
