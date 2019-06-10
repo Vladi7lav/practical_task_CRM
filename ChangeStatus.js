116c10b1785fb145fd0544b45495c89fd370c339
Vladislav Tsirulev <Vladislav.Tsirulev@columbusglobal.com>
2019-06-10 17:07:26 +0300

    Update ChangeStatus.js

--- ChangeStatus.js
+++ ChangeStatus.js
@@ -2,6 +2,7 @@ function ChangeStatus() {
     var phoneNumber = Xrm.Page.getAttribute("new_phone_number_recipient").getValue();
     var message = Xrm.Page.getAttribute("new_message").getValue(); 
     var status = Xrm.Page.getAttribute("statuscode").getValue();
+    //status: 100000000 - created, 100000001 - ready to send, 100000002 - sent
     if (phoneNumber === null || message === null || status === 100000001 || status === 100000002) return;
     else {
         Xrm.Page.getAttribute("statuscode").setValue(100000001);
