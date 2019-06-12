cac952347604925fca05d94176e91a2b1b02804f
Vladislav Tsirulev <Vladislav.Tsirulev@columbusglobal.com>
2019-06-12 11:26:35 +0300

    SendSMS update

--- SendSMS/SendSMS/Properties/AssemblyInfo.cs
+++ SendSMS/SendSMS/Properties/AssemblyInfo.cs
@@ -2,35 +2,34 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
+// General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("SentMSG")]
+[assembly: AssemblyTitle("SendSMS")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("SentMSG")]
+[assembly: AssemblyProduct("SendSMS")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
-
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("787a7508-2dff-4624-bd28-3b08cd11534d")]
+[assembly: Guid("7a480a4c-dcf7-4994-9faa-8f92a5cbb338")]
 
 // Version information for an assembly consists of the following four values:
 //
 //      Major Version
-//      Minor Version 
+//      Minor Version
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Build and Revision Numbers 
+// You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
---
+++ SendSMS/SendSMS/SendSMS.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Reflection;
+using System.Activities;
+using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Query;
+using Microsoft.Xrm.Sdk.Workflow;
+using Microsoft.Xrm.Sdk.Client;
+using Microsoft.Crm.Sdk.Messages;
+using Microsoft.Xrm.Sdk.Messages;
+using System.Xml;
+using System.Text.RegularExpressions;
+
+namespace SendSMS
+{
+    public sealed class SendSMS : CodeActivity
+    {
+        protected override void Execute(CodeActivityContext executionContext)
+        {
+            string id = new Random().Next(100000, 999999).ToString();
+            smsId.Set(executionContext, id);
+            string message = (string)messageText.Get(executionContext);
+            string number = (string)phoneNumber.Get(executionContext);
+            string[] phonenumbersArray = Regex.Split(number, "\\s*;\\s*");
+            number = "";
+
+            Regex regexObj = new Regex(@"[^\d]");
+
+            foreach (var num in phonenumbersArray)
+            {
+                number += (String.IsNullOrEmpty(num) ? "" : "+" + (string)regexObj.Replace(num, "") + "; ");
+            }
+            try
+            {
+                XmlWriter xmlWriter = XmlWriter.Create(@"\\CRM-TRAIN\Shared\newSMS.xml");
+
+                xmlWriter.WriteStartDocument();
+                xmlWriter.WriteStartElement("SMS");
+
+                xmlWriter.WriteStartElement("smsID");
+                xmlWriter.WriteString($"{id}");
+                xmlWriter.WriteEndElement();
+
+                xmlWriter.WriteStartElement("Phonenumber");
+                xmlWriter.WriteString($"{number}");
+                xmlWriter.WriteEndElement();
+
+                xmlWriter.WriteStartElement("Message");
+                xmlWriter.WriteString($"{message}");
+                xmlWriter.WriteEndElement();
+
+                xmlWriter.WriteEndDocument();
+                xmlWriter.Close();
+            }
+            catch (Exception e)
+            {
+                throw new InvalidWorkflowException(e.Message.ToString());
+            }
+        }
+        [Input("Phone number(s)")]
+        public InArgument<string> phoneNumber { get; set; }
+        [Input("Message text")]
+        public InArgument<string> messageText { get; set; }
+        [Output("smsID")]
+        public OutArgument<string> smsId { get; set; }
+    }
+}
